a75cd506c9eac5aaa3fb36f24e26a68f3f2b97bd
edson macedo de almeida <edson4323macedo2@gmail.com>
2023-10-01 21:10:03 -0300

    menu responsive

--- marketing criativo/script/script.js
+++ marketing criativo/script/script.js
@@ -1,6 +1,19 @@
 
-function clicar(){
-    var icon = window.document.getElementById('menu')
+function menuShow(){
 
-    icon.innerHTML = `<i class='bx bx-menu-alt-right'></i>`
+    let menuMobile = document.querySelector('.mobile-menu')
+    let icon = document.getElementById('icon')
+
+    if (menuMobile.classList.contains('ativo')){
+
+        menuMobile.classList.remove('ativo')
+        menuMobile.classList.add('inativo')
+        icon.innerHTML = `<i class='bx bx-menu'></i>`
+    }else{
+
+        menuMobile.classList.remove('inativo')
+        menuMobile.classList.add('ativo')
+        icon.innerHTML = `<i class='bx bx-menu-alt-right'></i>`
+    }
 }
+
